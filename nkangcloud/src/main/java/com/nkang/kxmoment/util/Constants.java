f1b108763f5737dcbb30e35de8279de77ba66b46
nkanggithub <camning_amanda@hotmail.com>
2016-11-07 10:24:05 +0800

    sss

--- nkangcloud/src/main/java/com/nkang/kxmoment/util/Constants.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/util/Constants.java
@@ -3,11 +3,17 @@ package com.nkang.kxmoment.util;
 public final class Constants {
 	private Constants(){}
 	
-	public static final String WECHAT_TOKEN = "SDFCTOKENSHANNKANG";
+/*	public static final String WECHAT_TOKEN = "SDFCTOKENSHANNKANG";
 	public static final String APP_ID = "wx3ad56259d9fbfe00";
-	public static final String APPSECRET = "593cd6c208938bc26f7d892c4ee8dbf1";
+	public static final String APPSECRET = "593cd6c208938bc26f7d892c4ee8dbf1";*/
 	//public static final String APP_ID = "wx3ad56259d9fbfe00";
 	//public static final String APPSECRET = "593cd6c208938bc26f7d892c4ee8dbf1";
+	
+	public static final String WECHAT_TOKEN = "SDFCTOKENSHANNKANG";
+	public static final String APP_ID = "wx19c8fd43a7b6525d";
+	public static final String APPSECRET = "513c09d69e91abb862e178536f2021a4";
+	
+	
 	public static final String BAIDU_APPKEY = "473c34a48268d02986e607cb5d7d564c";
 	public static final String US_ASCII = "US-ASCII";
 	public static final String GOOGLE_KEY = "AIzaSyB3bZ1N7XRiQu2M4ZB3QQMxd3VVLnzrRK4";
@@ -16,4 +22,6 @@ public final class Constants {
 	//https://maps.googleapis.com/maps/api/place/textsearch/json?query=%E9%87%8D%E5%BA%86%E6%83%A0%E6%99%AE&key=AIzaSyB3bZ1N7XRiQu2M4ZB3QQMxd3VVLnzrRK4
 	public static final String proxyInfo = "web-proxy.austin.hpecorp.net";
 	//web-proxy.austin.hpecorp.net:8080
+	// WESK: HYAMgd0odZSpLS8kmrGnoMezFoCTdxFQtWEnZL7lMIz
+	//https://shenan.duapp.com/coreServlet
 }
