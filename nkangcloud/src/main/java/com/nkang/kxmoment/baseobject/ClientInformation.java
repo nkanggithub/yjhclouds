e7eb4b6ba4f46468de94ab756023b8be39f7452b
nkanggithub <camning_amanda@hotmail.com>
2016-11-07 10:17:54 +0800

    saved

---
+++ nkangcloud/src/main/java/com/nkang/kxmoment/baseobject/ClientInformation.java
@@ -0,0 +1,27 @@
+package com.nkang.kxmoment.baseobject;
+
+public class ClientInformation {
+	private String ClientID;
+	private String ClientIdentifier;
+	private String ClientDescription;
+	public String getClientID() {
+		return ClientID;
+	}
+	public void setClientID(String clientID) {
+		ClientID = clientID;
+	}
+	public String getClientIdentifier() {
+		return ClientIdentifier;
+	}
+	public void setClientIdentifier(String clientIdentifier) {
+		ClientIdentifier = clientIdentifier;
+	}
+	public String getClientDescription() {
+		return ClientDescription;
+	}
+	public void setClientDescription(String clientDescription) {
+		ClientDescription = clientDescription;
+	}
+	
+	
+}
