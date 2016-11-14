9eb06e6e1736b18a2957d0620e6422dee8f9c376
nkanggithub <camning_amanda@hotmail.com>
2016-11-14 16:52:16 +0800

    save

--- nkangcloud/src/main/java/com/nkang/kxmoment/controller/UserProfileController.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/controller/UserProfileController.java
@@ -5,9 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.nkang.kxmoment.baseobject.GeoLocation;
 import com.nkang.kxmoment.util.RestUtils;
 
 
@@ -22,6 +22,17 @@ public class UserProfileController {
 		location=location.substring(0, 3);
 		String weather= RestUtils.getWeatherInform(location);
 		return weather;
+	}
+	@RequestMapping(value="/getLocation",produces="text/html;charset=UTF-8")
+	@ResponseBody  
+	public String getLocation(HttpServletRequest request, HttpServletResponse response){
+		String uid=request.getParameter("uid");
+		GeoLocation loc= RestUtils.callGetDBUserGeoInfo(uid);
+		String curLoc = RestUtils.getUserCurLocWithLatLng(loc.getLAT() , loc.getLNG()); 
+		if(curLoc!=null&&!"".equals(curLoc)){
+			request.getSession().setAttribute("location",curLoc);
+		}
+		return curLoc;
 	}
 	
 }
