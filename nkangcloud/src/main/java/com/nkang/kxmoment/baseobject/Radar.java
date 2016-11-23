b62c3427675047773d8343acfe1389e21f63bbfd
nkanggithub <camning_amanda@hotmail.com>
2016-11-23 10:19:01 +0800

    雷达图对象

--- nkangcloud/src/main/java/com/nkang/kxmoment/baseobject/Radar.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/baseobject/Radar.java
@@ -4,16 +4,24 @@ public class Radar {
 
 	public String axis;
 	public double value;
+	public double count;
+	public double getCount() {
+		return count;
+	}
+	public void setCount(double count) {
+		this.count = count;
+	}
 	public String getAxis() {
 		return axis;
 	}
 	public void setAxis(String axis) {
 		this.axis = axis;
 	}
-	public Radar(String axis, double value) {
+	public Radar(String axis, double value,double count) {
 		super();
 		this.axis = axis;
 		this.value = value;
+		this.count=count;
 	}
 	public double getValue() {
 		return value;
