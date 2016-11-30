bc80c56b0739de983f6e03f82291094ece46b8de
nkanggithub <camning_amanda@hotmail.com>
2016-11-30 14:37:06 +0800

    cfws

---
+++ nkangcloud/src/main/java/com/nkang/kxmoment/baseobject/OrgCountryCode.java
@@ -0,0 +1,48 @@
+package com.nkang.kxmoment.baseobject;
+
+public class OrgCountryCode {
+	String countryCode;
+	String countryName;
+	String totalCount;
+	String customerCount;
+	String partnerCount;
+	String competitorCount;
+	public OrgCountryCode(){}
+	public String getCountryCode() {
+		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public String getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount(String totalCount) {
+		this.totalCount = totalCount;
+	}
+	public String getCustomerCount() {
+		return customerCount;
+	}
+	public void setCustomerCount(String customerCount) {
+		this.customerCount = customerCount;
+	}
+	public String getPartnerCount() {
+		return partnerCount;
+	}
+	public void setPartnerCount(String partnerCount) {
+		this.partnerCount = partnerCount;
+	}
+	public String getCompetitorCount() {
+		return competitorCount;
+	}
+	public void setCompetitorCount(String competitorCount) {
+		this.competitorCount = competitorCount;
+	}
+	
+}
