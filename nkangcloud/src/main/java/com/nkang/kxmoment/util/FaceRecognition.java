60023600afe3cba99f26a457191eabd14e3e3216
nkanggithub <camning_amanda@hotmail.com>
2016-12-26 11:01:16 +0800

    added

--- nkangcloud/src/main/java/com/nkang/kxmoment/util/FaceRecognition.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/util/FaceRecognition.java
@@ -16,7 +16,7 @@ public class FaceRecognition {
 
  public String goface(String picurl) 
  {
-	 /*
+	 /*	FaceRecognition - Basic
 	  * https://dev.projectoxford.ai/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236
 	  * https://dev.projectoxford.ai/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236/console
 	  */
@@ -41,6 +41,38 @@ public class FaceRecognition {
          if (entity != null) 
          {
         	 
+        	 ret = EntityUtils.toString(entity);
+         }
+     }
+     catch (Exception e)
+     {
+         System.out.println(e.getMessage());
+     }
+     return ret;
+ }
+ 
+ public String gofaceEmotion(String picurl) 
+ {
+	 /* FaceRecognition - Emotion
+	  * https://dev.projectoxford.ai/docs/services/5639d931ca73072154c1ce89/operations/563b31ea778daf121cc3a5fa/console
+	  */
+     HttpClient httpclient = HttpClients.createDefault();
+     String ret = "";
+     try
+     {
+    	 URIBuilder builder = new URIBuilder("https://api.projectoxford.ai/emotion/v1.0/recognize");
+         URI uri = builder.build();
+         HttpPost request = new HttpPost(uri);
+         request.setHeader("Content-Type", "application/json");
+         request.setHeader("Ocp-Apim-Subscription-Key", "26ba3dbf9bb54c37a984103de20ab33b");
+         
+         // Request body
+         StringEntity reqEntity = new StringEntity("{\"url\":\"" +picurl + "\"}");
+         request.setEntity(reqEntity);
+         HttpResponse response = httpclient.execute(request);
+         HttpEntity entity = response.getEntity();
+         if (entity != null) 
+         {
         	 ret = EntityUtils.toString(entity);
          }
      }
