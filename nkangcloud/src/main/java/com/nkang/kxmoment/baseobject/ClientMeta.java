0f83eefc670f6957fb6c898b3f81658cba2e2f20
nkanggithub <camning_amanda@hotmail.com>
2017-01-05 13:33:18 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/baseobject/ClientMeta.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/baseobject/ClientMeta.java
@@ -5,6 +5,28 @@ public class ClientMeta {
 	private String ClientLogo;
 	private String ClientName;
 	private String ClientSubName;
+	private String clientThemeColor;
+	private String clientStockCode;
+	private String clientActive;
+	
+	public String getClientThemeColor() {
+		return clientThemeColor;
+	}
+	public void setClientThemeColor(String clientThemeColor) {
+		this.clientThemeColor = clientThemeColor;
+	}
+	public String getClientStockCode() {
+		return clientStockCode;
+	}
+	public void setClientStockCode(String clientStockCode) {
+		this.clientStockCode = clientStockCode;
+	}
+	public String getClientActive() {
+		return clientActive;
+	}
+	public void setClientActive(String clientActive) {
+		this.clientActive = clientActive;
+	}
 	public String getClientCopyRight() {
 		return ClientCopyRight;
 	}
