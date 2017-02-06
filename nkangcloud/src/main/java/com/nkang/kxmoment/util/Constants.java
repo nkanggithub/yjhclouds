383d3aaab1e722c54c2b9c53f3dd5109641d3157
nkanggithub <camning_amanda@hotmail.com>
2017-02-06 14:42:15 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/util/Constants.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/util/Constants.java
@@ -3,14 +3,8 @@ package com.nkang.kxmoment.util;
 public final class Constants {
 	private Constants(){}
 	
-/*	public static final String WECHAT_TOKEN = "SDFCTOKENSHANNKANG";
-	public static final String APP_ID = "wx3ad56259d9fbfe00";
-	public static final String APPSECRET = "593cd6c208938bc26f7d892c4ee8dbf1";*/
-	//public static final String APP_ID = "wx3ad56259d9fbfe00";
-	//public static final String APPSECRET = "593cd6c208938bc26f7d892c4ee8dbf1";
-	
 	public static final String WECHAT_TOKEN = "SDFCTOKENSHANNKANG";
-	public static final String APP_ID = "wx19c8fd43a7b6525d";
+	public static final String APP_ID = "wxaa0a9bfa29863d87"; //YJH
 	public static final String APPSECRET = "513c09d69e91abb862e178536f2021a4";
 	
 	
@@ -18,10 +12,8 @@ public final class Constants {
 	public static final String US_ASCII = "US-ASCII";
 	public static final String GOOGLE_KEY = "AIzaSyB3bZ1N7XRiQu2M4ZB3QQMxd3VVLnzrRK4";
 	public static final String convertString = "<%=value%>";
-	//https://maps.googleapis.com/maps/api/geocode/json?address=aaaa6&key=AIzaSyB3bZ1N7XRiQu2M4ZB3QQMxd3VVLnzrRK4
-	//https://maps.googleapis.com/maps/api/place/textsearch/json?query=%E9%87%8D%E5%BA%86%E6%83%A0%E6%99%AE&key=AIzaSyB3bZ1N7XRiQu2M4ZB3QQMxd3VVLnzrRK4
 	public static final String proxyInfo = "web-proxy.austin.hpecorp.net";
-	public static final String baehost = "shenan.duapp.com";
+	public static final String baehost = "yongjiahe.duapp.com";
 	public static final String wechatapihost = "api.weixin.qq.com";
 	public static final String baiduapihost = "api.map.baidu.com";
 	//web-proxy.austin.hpecorp.net:8080
@@ -29,7 +21,7 @@ public final class Constants {
 	//https://shenan.duapp.com/coreServlet
 	
 	/**
-	 * MongoDB 集合名字
+	 * MongoDB 
 	 */
 	public final class MONGO_CLN_NAMES{
 		public static final String KM = "KMPool";
