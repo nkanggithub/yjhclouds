745af9bb92c1dd970bef94b13d1420683db96882
nkanggithub <camning_amanda@hotmail.com>
2017-02-14 23:03:10 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/service/CoreService.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/service/CoreService.java
@@ -273,7 +273,7 @@ public class CoreService
 						article.setTitle("胖和微应用");
 						article.setDescription("我的应用");
 						article.setPicUrl("https://c.ap1.content.force.com/servlet/servlet.ImageServer?id=0159000000DlU0c&oid=00D90000000pkXM");
-						article.setUrl("http://"+Constants.baehost+"/mdm/welcome.html");
+						article.setUrl("http://"+Constants.baehost+"/mdm/welcome.jsp?UID=" + fromUserName);
 						articleList.add(article);
 						Article article2 = new Article();
 						article2.setTitle("胖和微应用");
