a164db6d8ed4c0c197046bde81f2c70843213473
nkanggithub <camning_amanda@hotmail.com>
2017-02-21 21:18:51 +0800

    saved

--- nkangcloud/src/main/java/com/nkang/kxmoment/service/CoreService.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/service/CoreService.java
@@ -464,20 +464,20 @@ public class CoreService
 						articleList.clear();
 						Article article = new Article();
 						article.setTitle("永佳和塑胶有限公司仓库导航");
-						article.setDescription("永佳和塑胶有限公司仓库导航");
+						article.setDescription("永佳和塑胶有限公司仓库为您导航");
 						article.setPicUrl(inventoryPic);
 						article.setUrl("http://map.baidu.com/mobile");
 						articleList.add(article);
 						
 						Article articlenav1 = new Article();
-						articlenav1.setTitle("公运库-九龙坡区铁路村198号-工作时间8:30AM-17:30PM");
+						articlenav1.setTitle("公运库\n九龙坡区铁路村198号\n工作时间8:30AM-17:30PM\n电话68526840");
 						articlenav1.setDescription("公运库-电话68526840");
 						articlenav1.setPicUrl(navPic);
 						articlenav1.setUrl("http://map.baidu.com/mobile/webapp/index/index#search/search/qt=s&wd=%E4%B9%9D%E9%BE%99%E5%9D%A1%E5%8C%BA%E9%93%81%E8%B7%AF%E6%9D%91198%E5%8F%B7&c=132&searchFlag=bigBox&version=5&exptype=dep&src_from=webapp_all_bigbox&src=0/vt=map");
 						articleList.add(articlenav1);
 						
 						Article articlenav2 = new Article();
-						articlenav2.setTitle("囤鑫中石化库-港城工业园区C区-工作时间8:30AM-17:20PM");
+						articlenav2.setTitle("囤鑫中石化库\n港城工业园区C区\n工作时间8:30AM-17:20PM\n囤鑫中石化库-电话88301978");
 						articlenav2.setDescription("囤鑫中石化库-电话88301978");
 						articlenav2.setPicUrl(navPic);
 						articlenav2.setUrl("http://map.baidu.com/mobile/webapp/index/index#search/search/qt=s&wd=%E6%B8%AF%E5%9F%8E%E5%B7%A5%E4%B8%9A%E5%9B%AD%E5%8C%BAc%E5%8C%BA&c=132&searchFlag=bigBox&version=5&exptype=dep&src_from=webapp_all_bigbox&wd2=%E9%87%8D%E5%BA%86%E5%B8%82%E6%B1%9F%E5%8C%97%E5%8C%BA&sug_forward=c1f324f93a6385b832f71d3b&src=1/vt=map");
@@ -491,7 +491,7 @@ public class CoreService
 						articleList.add(articlenav3);
 						
 						Article articlenav4 = new Article();
-						articlenav4.setTitle("天地金中心库-重庆沙坪坝区土主镇西部物流园区中石油仓储中心-工作时间8:30AM-18:30PM");
+						articlenav4.setTitle("天地金中心库\n重庆沙坪坝区土主镇西部物流园区中石油仓储中心\n工作时间8:30AM-18:30PM\n电话65329881");
 						articlenav4.setDescription("天地金中心库-电话65329881");
 						articlenav4.setPicUrl(navPic);
 						articlenav4.setUrl("http://map.baidu.com/mobile/webapp/index/index#search/search/qt=s&wd=%E8%A5%BF%E9%83%A8%E7%89%A9%E6%B5%81%E5%9B%AD&c=132&searchFlag=bigBox&version=5&exptype=dep&src_from=webapp_all_bigbox&wd2=%E9%87%8D%E5%BA%86%E5%B8%82%E9%87%8D%E5%BA%86%E5%B8%82&sug_forward=9434cf40ee8a5a6d83a00c0f&src=1/vt=map");
