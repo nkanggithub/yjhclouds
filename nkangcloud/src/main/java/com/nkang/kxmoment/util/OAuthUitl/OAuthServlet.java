621f3b8dd9b51c49c0e4a2997663dca0c9d50739
nkanggithub <camning_amanda@hotmail.com>
2017-07-12 10:44:03 +0800

    filter

--- nkangcloud/src/main/java/com/nkang/kxmoment/util/OAuthUitl/OAuthServlet.java
+++ nkangcloud/src/main/java/com/nkang/kxmoment/util/OAuthUitl/OAuthServlet.java
@@ -1,5 +1,7 @@
 package com.nkang.kxmoment.util.OAuthUitl;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-
-import com.nkang.kxmoment.util.Constants;
 
 //import com.souvc.weixin.pojo.SNSUserInfo;
 //import com.souvc.weixin.pojo.WeixinOauth2Token;
@@ -29,7 +29,7 @@ import com.nkang.kxmoment.util.Constants;
 //public class OAuthServlet extends HttpServlet {
 public class OAuthServlet implements Filter  {
     private Logger log = Logger.getLogger(this.getClass());
- 
+   
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		// TODO Auto-generated method stub
@@ -42,36 +42,67 @@ public class OAuthServlet implements Filter  {
 
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
+       // String getRequestURI =((HttpServletRequest) request).getServletPath();
+
+       // String getRequestURL =((HttpServletRequest) request).getRequestURL().toString();
 
         // 用户同意授权后，能获取到code
         String code = request.getParameter("code");
         String state = request.getParameter("state");
+    	HttpServletRequest req=(HttpServletRequest) request;
+        String uri= req.getRequestURI(); //uri就是获取到的连接地址!
         //String accessToken=null;
         request.setAttribute("code", code);
-        if(code!=null&&code!=""){
+        if(code!=null&&code!=""&&!"authdeny".equals(code)){
         	 // 用户同意授权
-            if (!"authdeny".equals(code)) {
-                // 获取网页授权access_token
-            	WeixinOauth2Token weixinOauth2Token = AdvancedUtil.getOauth2AccessToken(Constants.APP_ID, Constants.APPSECRET, code);
-                // 网页授权接口访问凭证
-                if(weixinOauth2Token!=null){
-                	 String accessToken = weixinOauth2Token.getAccessToken();
-                	 // 用户标识
-                     String openId = weixinOauth2Token.getOpenId();
-                     // 获取用户信息
-                     SNSUserInfo snsUserInfo = AdvancedUtil.getSNSUserInfo(accessToken, openId);
-                     // 设置要传递的参数
-                     request.setAttribute("snsUserInfo", snsUserInfo);
-                     request.setAttribute("state", state);
-                }
+        	//log.info("Start call weixinOauth2Token .......");
+            // 获取网页授权access_token
+            WeixinOauth2Token weixinOauth2Token = AdvancedUtil.getOauth2AccessToken("wx19c8fd43a7b6525d", "513c09d69e91abb862e178536f2021a4", code);
+            //log.info("end call weixinOauth2Token .......=="+weixinOauth2Token.getAccessToken());
+            // 网页授权接口访问凭证
+            if(weixinOauth2Token!=null){
+            	 String accessToken = weixinOauth2Token.getAccessToken();
+            	 // log.info("accessToken .--->"+accessToken);
+            	 // 用户标识
+                 String openId = weixinOauth2Token.getOpenId();
+                 //log.info("openId .--->"+openId);
+                 // 获取用户信息
+                 SNSUserInfo snsUserInfo = AdvancedUtil.getSNSUserInfo(accessToken, openId);
+                // log.info("snsUserInfo .--->"+snsUserInfo.getOpenId());
+                 // 设置要传递的参数
+                 request.setAttribute("snsUserInfo", snsUserInfo);
+                 request.setAttribute("state", state);
             }
-            log.info("end doFilter.......");
         }
+        SNSUserInfo user = (SNSUserInfo)request.getAttribute("snsUserInfo"); 
+    	if(user==null){
+    	 Enumeration  enum1=req.getParameterNames();
+    	 String value="";
+    	 while(enum1.hasMoreElements()){    
+    		 String  paramName=(String)enum1.nextElement();    
+    		 if(!"from".equals(paramName)&&!"isappinstalled".equals(paramName)&&!"code".equals(paramName)){
+        		 if("".equals(value)){
+        			 value+="?";
+        		 }else{
+        			 value+="&";
+        		 }
+                 value+=paramName;
+                 value+="=";
+                 value+=req.getParameter(paramName);
+    		 }
+    	 }  
+    	 log.info("===============url:"+uri+value);
+    	 uri=java.net.URLEncoder.encode(uri+value, "utf-8");
+    	 log.info("===============encode(uri):"+uri);
+    	((HttpServletResponse) response).sendRedirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx19c8fd43a7b6525d&redirect_uri=http%3A%2F%2Fshenan.duapp.com"+uri+"&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
+    	}
+        
+        log.info("end doFilter.......");
+        chain.doFilter(request, response);
        
        
         // 跳转到index.jsp
 //        request.getRequestDispatcher("index.jsp").forward(request, response);
-        chain.doFilter(request, response);
 		
 	}
 	@Override
